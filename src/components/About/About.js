effe0efc3b0c3c42470b94aa320a42af820c3d99
wsiddons <wsiddons11@gmail.com>
2022-10-20 17:18:12 -0400

    added some fun stuff

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function About() {
+    const [willImgState, setWillImgState] = useState('./faces/pixelWill.png')
+    const [noelImgState, setNoelImgState] = useState('./faces/pixelNoel.png')
+    const [billImgState, setBillImgState] = useState('./faces/pixelBill.png')
+    const [drewImgState, setDrewImgState] = useState('./faces/pixelDrew.png')
+    const [bilalImgState, setBilalImgState] = useState('./faces/pixelBilal.png')
+
     return (
         <>
             <div className='about-container'>
@@ -14,27 +20,27 @@ function About() {
                 <div className='dev-container'>
 
                     <div className='bio-container'>
-                        <img width='150px' src={require('./faces/pixelWill.png')} alt='pixelWill' />
+                        <img width='150px' onMouseLeave={() => setWillImgState('./faces/pixelWill.png')} onMouseEnter={() => setWillImgState('./faces/pixelWill-mouth.png')} src={require(`${willImgState}`)} alt='pixelWill' />
                         <h5>Will Siddons</h5>
                         <p><a href='https://www.linkedin.com/in/willsiddons/'>LinkedIn</a></p>
                     </div>
                     <div className='bio-container'>
-                        <img width='150px' height='150px' src={require('./faces/pixelBill.png')} alt='pixelBill' />
+                        <img width='150px' height='150px' onMouseLeave={() => setBillImgState('./faces/pixelBill.png')} onMouseEnter={() => setBillImgState('./faces/pixelBill-cat.png')} src={require(`${billImgState}`)} alt='pixelBill' />
                         <h5>Bill Munkacsy</h5>
                         <p><a href='https://www.linkedin.com/in/williammunkacsy/'>LinkedIn</a></p>
                     </div>
                     <div className='bio-container'>
-                        <img width='150px' src={require('./faces/pixelNoel.png')} alt='pixelNoel' />
+                        <img width='150px' onMouseLeave={() => setNoelImgState('./faces/pixelNoel.png')} onMouseEnter={() => setNoelImgState('./faces/pixelNoel-pirate.png')} src={require(`${noelImgState}`)} alt='pixelNoel' />
                         <h5>Noel Benford</h5>
                         <p><a href='https://www.linkedin.com/in/noelbenford/'>LinkedIn</a></p>
                     </div>
                     <div className='bio-container'>
-                        <img width='150px' height='150px' src={require('./faces/pixelDrew.png')} alt='pixelDrew' />
+                        <img width='150px' height='150px' onMouseLeave={() => setDrewImgState('./faces/pixelDrew.png')} onMouseEnter={() => setDrewImgState('./faces/pixelDrew-dog.png')} src={require(`${drewImgState}`)} alt='pixelDrew' />
                         <h5>Andrew Dobson</h5>
                         <p><a href='https://www.linkedin.com/in/tandrewlopez/'>LinkedIn</a></p>
                     </div>
                     <div className='bio-container'>
-                        <img width='150px' src={require('./faces/pixelBilal.png')} alt='pixelBilal' />
+                        <img width='150px' onMouseLeave={() => setBilalImgState('./faces/pixelBilal.png')} onMouseEnter={() => setBilalImgState('./faces/pixelBilal-crown.png')} src={require(`${bilalImgState}`)} alt='pixelBilal' />
                         <h5>Bilal Abbas</h5>
                         <p><a href='https://www.linkedin.com/in/babbasuk/'>LinkedIn</a></p>
                     </div>
