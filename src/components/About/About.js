4d2522433f1173aa00f98685903c57c906142ff5
wsiddons <wsiddons11@gmail.com>
2022-10-21 15:42:12 -0400

    added fun stuff

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,12 +7,39 @@ function About() {
     const [drewImgState, setDrewImgState] = useState('./faces/pixelDrew.png')
     const [bilalImgState, setBilalImgState] = useState('./faces/pixelBilal.png')
 
+    const [imgState, setImgState] = useState([
+        willImgState,
+        noelImgState,
+        billImgState,
+        drewImgState,
+        bilalImgState
+    ])
+
+    const handleMouseLeave = () => {
+        setWillImgState('./faces/pixelWill.png')
+        setNoelImgState('./faces/pixelNoel.png')
+        setBillImgState('./faces/pixelBill.png')
+        setDrewImgState('./faces/pixelDrew.png')
+        setBilalImgState('./faces/pixelBilal.png')
+    }
+
+    const handleMouseEnter = () => {
+        setWillImgState('./faces/pixelWill-eyes.png')
+        setNoelImgState('./faces/pixelNoel-eyes.png')
+        setBillImgState('./faces/pixelBill-eyes.png')
+        setDrewImgState('./faces/pixelDrew-eyes.png')
+        setBilalImgState('./faces/pixelBilal-eyes.png')
+    }
+
+
     return (
         <>
             <div className='about-container'>
                 <div className='about-game-container'>
                     <h3>About the Game</h3>
-                    <h5><a href='https://github.com/DrTeethCapstone/capstone-ml-three-firebase'>Project Github</a></h5>
+                    <h5 ><a
+                        onMouseLeave={handleMouseLeave} onMouseEnter={handleMouseEnter}
+                        href='https://github.com/DrTeethCapstone/capstone-ml-three-firebase'>Project Github</a></h5>
                     <p>Linkuistix is a word association game that uses an artificial intelligence trained by machine learning.
 
                         The AI allows us to present a set of words to the player and then rank how closely each of the presented words is associated with the player's input. If the player inputs a word that is closely associated with the targeted word the player gets points added to their score and a new set of words is presented. The time the player has to input words decreases the more they play until eventually the player is unable to enter a closely associated word in time.</p>
@@ -24,27 +51,27 @@ function About() {
                     <div className='bio-container'>
                         <img width='150px' onMouseLeave={() => setWillImgState('./faces/pixelWill.png')} onMouseEnter={() => setWillImgState('./faces/pixelWill-mouth.png')} src={require(`${willImgState}`)} alt='pixelWill' />
                         <h5>Will Siddons</h5>
-                        <p><a href='https://www.linkedin.com/in/willsiddons/'>LinkedIn</a></p>
+                        <p onMouseLeave={() => setWillImgState('./faces/pixelWill.png')} onMouseEnter={() => setWillImgState('./faces/pixelWill-mouth.png')}><a href='https://www.linkedin.com/in/willsiddons/'>LinkedIn</a></p>
                     </div>
                     <div className='bio-container'>
                         <img width='150px' height='150px' onMouseLeave={() => setBillImgState('./faces/pixelBill.png')} onMouseEnter={() => setBillImgState('./faces/pixelBill-cat.png')} src={require(`${billImgState}`)} alt='pixelBill' />
                         <h5>Bill Munkacsy</h5>
-                        <p><a href='https://www.linkedin.com/in/williammunkacsy/'>LinkedIn</a></p>
+                        <p onMouseLeave={() => setBillImgState('./faces/pixelBill.png')} onMouseEnter={() => setBillImgState('./faces/pixelBill-cat.png')}><a href='https://www.linkedin.com/in/williammunkacsy/'>LinkedIn</a></p>
                     </div>
                     <div className='bio-container'>
                         <img width='150px' onMouseLeave={() => setNoelImgState('./faces/pixelNoel.png')} onMouseEnter={() => setNoelImgState('./faces/pixelNoel-pirate.png')} src={require(`${noelImgState}`)} alt='pixelNoel' />
                         <h5>Noel Benford</h5>
-                        <p><a href='https://www.linkedin.com/in/noelbenford/'>LinkedIn</a></p>
+                        <p onMouseLeave={() => setNoelImgState('./faces/pixelNoel.png')} onMouseEnter={() => setNoelImgState('./faces/pixelNoel-pirate.png')}><a href='https://www.linkedin.com/in/noelbenford/'>LinkedIn</a></p>
                     </div>
                     <div className='bio-container'>
                         <img width='150px' height='150px' onMouseLeave={() => setDrewImgState('./faces/pixelDrew.png')} onMouseEnter={() => setDrewImgState('./faces/pixelDrew-dog.png')} src={require(`${drewImgState}`)} alt='pixelDrew' />
                         <h5>Andrew Dobson</h5>
-                        <p><a href='https://www.linkedin.com/in/tandrewlopez/'>LinkedIn</a></p>
+                        <p onMouseLeave={() => setDrewImgState('./faces/pixelDrew.png')} onMouseEnter={() => setDrewImgState('./faces/pixelDrew-dog.png')}><a href='https://www.linkedin.com/in/tandrewlopez/'>LinkedIn</a></p>
                     </div>
                     <div className='bio-container'>
                         <img width='150px' onMouseLeave={() => setBilalImgState('./faces/pixelBilal.png')} onMouseEnter={() => setBilalImgState('./faces/pixelBilal-crown.png')} src={require(`${bilalImgState}`)} alt='pixelBilal' />
                         <h5>Bilal Abbas</h5>
-                        <p><a href='https://www.linkedin.com/in/babbasuk/'>LinkedIn</a></p>
+                        <p onMouseLeave={() => setBilalImgState('./faces/pixelBilal.png')} onMouseEnter={() => setBilalImgState('./faces/pixelBilal-crown.png')}><a href='https://www.linkedin.com/in/babbasuk/'>LinkedIn</a></p>
                     </div>
                 </div>
             </div>
