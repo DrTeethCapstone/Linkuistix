c6c9556b4bc6cbc4bff998f845ebd6724671d83a
nbenford <noel.benford@gmail.com>
2022-10-21 10:44:32 -0400

    init Web Worker implementation

--- src/game/GameContainer/InputContainer/InputText.js
+++ src/game/GameContainer/InputContainer/InputText.js
@@ -1,22 +1,29 @@
-import * as PIXI from "pixi.js";
-import * as tf from "@tensorflow/tfjs";
-import * as use from "@tensorflow-models/universal-sentence-encoder";
+import * as PIXI from 'pixi.js';
+import * as tf from '@tensorflow/tfjs';
+import * as use from '@tensorflow-models/universal-sentence-encoder';
 
 //CREATE A NEW INSTANCE OF A USER INPUT FIELD
 export class InputText extends PIXI.Text {
   constructor(parent = null) {
-    super("type here", {
-      fontFamily: "Press Start 2P",
+    super('type here', {
+      fontFamily: 'Press Start 2P',
       fontSize: 24,
       fill: 0xebd25b,
-      align: "center",
+      align: 'center',
     });
 
     //DEFAULTS
     this.parent = parent;
-    this.userGuess = "";
+    this.userGuess = '';
     this.interactive = true;
     this.enabled = false;
+
+    //Our TF web worker
+    this.worker = new Worker(new URL('./TF_Worker.js', import.meta.url), {
+      type: 'module',
+    });
+
+    this.TFOutput = [];
 
     //DON'T HAVE ACCESS TO THESE VALUES UNTIL SPECIFIC METHODS ARE CALLED
     // this.model = null;
@@ -27,7 +34,7 @@ export class InputText extends PIXI.Text {
       const containerBG = new PIXI.Sprite(PIXI.Texture.WHITE);
       // console.dir('container', container)
       containerBG.tint = 0x1f1f1f;
-      containerBG.alpha = 0.8
+      containerBG.alpha = 0.8;
       containerBG.width = this.parent.width;
       containerBG.height = this.parent.height / 2;
       container.position.y = -containerBG.height;
@@ -41,7 +48,7 @@ export class InputText extends PIXI.Text {
     }
 
     //ONLY ENABLED IF USER CLICKS ON FIELD
-    this.on("pointerdown", (e) => {
+    this.on('pointerdown', (e) => {
       this.style.fill = 0x0eb3e1;
       this.setupKeyboardListener();
     });
@@ -52,9 +59,9 @@ export class InputText extends PIXI.Text {
   setupKeyboardListener() {
     if (!this.enabled) {
       this.enabled = true;
-      window.addEventListener("keydown", (e) => {
+      window.addEventListener('keydown', (e) => {
         this.updateInputText(e, this);
-      })
+      });
     }
   }
 
@@ -77,12 +84,11 @@ export class InputText extends PIXI.Text {
     return true;
   }
 
-  GRU
+  GRU;
 
   //KEYBOARD
   updateInputText(e, me) {
-    if (e.key === "Enter") {
-
+    if (e.key === 'Enter') {
       // this.parent.parent.parent.children[4].resetTimer()
 
       //ARRAY OF WORD OBJECTS
@@ -94,31 +100,50 @@ export class InputText extends PIXI.Text {
       const tensorWords = words.map((word) => word.text);
 
       const validation = {
-        targetString: targetWord.text,
+        targetString: targetWord._text,
         inputString: this.userGuess.toLowerCase(),
         target: targetWord,
       };
       const prevWordObject = this.parent.parent.children[2].children[1];
 
       if (this.validateWordInput(validation)) {
-        this.speakToTensor(
-          [this.userGuess],
+        //   this.speakToTensor(
+        //     [this.userGuess],
+        //     tensorWords,
+        //     words,
+        //     prevWordObject
+        //   );
+        // }
+
+        this.worker.postMessage({
+          userInput: [this.userGuess],
           tensorWords,
-          words,
-          prevWordObject
-        );
+        });
+
+        //listen for a response and set the scores
+        this.worker.addEventListener('message', async ({ data }) => {
+          const { TFOutput } = data;
+          console.log('returned from worker: ', TFOutput);
+          this.TFOutput = TFOutput;
+          for (let i = 0; i < this.TFOutput.length; i++) {
+            words[i].similarityScore = this.TFOutput[i];
+          }
+
+          console.log(words);
+
+          this.assignSimilarityIndex(words, prevWordObject);
+
+          prevWordObject.updateWord(this.userGuess);
+          this.userGuess = '';
+          me.text = '';
+        });
       }
-
-      prevWordObject.updateWord(this.userGuess);
-
-      this.userGuess = "";
-      me.text = "";
-    } else if (e.key === "Backspace") {
+    } else if (e.key === 'Backspace') {
       this.userGuess = this.userGuess.slice(0, this.userGuess.length - 1);
       me.text = this.userGuess;
     } else {
       //ONLY ALPHABET CHARACTERS AND SPACES ARE ACCEPTED
-      if (this.isLetter(e.key) || e.key === " ") {
+      if (this.isLetter(e.key) || e.key === ' ') {
         this.userGuess += e.key.toLowerCase();
         me.text = this.userGuess;
       }
