99eee6df5131536fe68f0fefea54f3506d67a421
Bill Munkacsy <billmunkacsy@gmail.com>
2022-10-24 12:09:43 -0400

    more fixes

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -49,14 +49,6 @@ function LogIn({ setShowSidebar, sketch }) {
       await loginAsGuest();
       setShowSidebar(true);
       navigate('/game');
-<<<<<<< HEAD
-=======
-      sketch.gameMenu.setUser({
-        email: currentUser.email,
-        id: currentUser.uid,
-        username: currentUser.displayName
-      })
->>>>>>> main
     } catch (error) {
       setError('failed to log in');
     }
